4968fc040ce573cbfca3dec3bafce1fdfdb5b968
NinaZC <147039359+NinaZC@users.noreply.github.com>
2023-11-14 15:53:08 +0100

    WallFollow_Nina.ino

--- P1.ino
+++ P1.ino
@@ -1,26 +1,30 @@
-
 #include <Wire.h>
 #include <Zumo32U4.h> // nødvendige 
 
-
-Zumo32U4ProximitySensors proxSensor; // sensore på siden
-Zumo32U4Motors motor; // nødvendig for at zumo kan kører
-Zumo32U4ButtonA buttonA; // 
+Zumo32U4ProximitySensors proxSensor;
+Zumo32U4Motors motor;
+Zumo32U4ButtonA buttonA;
 Zumo32U4OLED display;
 
+//Skal ændres
+int min = 30;
+int max = 40;
 
 void setup() {
   proxSensor.initThreeSensors();
-  proxSensor.setBrightnessLevels(50,500);//første er level, næste er count brightness
-    while(buttonA.isPressed() != 1){//når bA ikke er trykket ned, så kører den i wloop.
+  proxSensor.setBrightnessLevels(70,700);//første er level, næste er count brightness
+  buttonA.waitForButton(); //den er god at have med, fordi loopet starter først når der er trykket 
+  buttonA.getSingleDebouncedRelease();
+  while(buttonA.getSingleDebouncedRelease() == true){
     proxSensor.read();
-    int proxRight = proxSensor.countsRightWithRightLeds();
+    int proxRight = proxSensor.countsRightWithRightLeds ();
     display.clear();
     display.gotoXY(0,0); //levels aflæses før press A, når det er sat i whileloop i setup
     display.print(proxRight); 
-  }
-  //buttonA.getSingleDebouncedPress(); //den er god at have med, fordi loopet starter først når der er trykket 
-  motor.setSpeeds(100,100);
+  } 
+  proxSensor.read();
+  min = proxSensor.countsRightWithRightLeds();
+  motor.setSpeeds(40,40);
   // put your setup code here, to run once:
 
 }
@@ -33,21 +37,29 @@ void loop() {
   display.clear();
   display.gotoXY(0,0);
   display.print(proxRight);
-  if (proxRight >=30){
-    motor.setSpeeds(50,80);// lav hastighed giver mindre overslag i ønsket distance (proportionel regulering)
-   
-    
 
-    
+  if (proxRight > min && proxRight < max){
+    proxSensor.read();
+    motor.setSpeeds(50,80);// lav hastighed giver mindre overslag i ønsket distance (proportionel regulering)
+    proxSensor.read();
   }
-  else if (proxRight <30){
+
+  else if (proxRight < min){
+    proxSensor.read();
     motor.setSpeeds(80,50);
-    
-    
+    proxSensor.read();
   }
-  else if (proxRight == 30){
-    motor.setSpeeds(50,50);
-    
+
+  else if (proxRight > max){
+    proxSensor.read();
+    motor.setSpeeds(-60,60);
+    proxSensor.read();
+  }
+
+  else if (proxRight == min){
+    proxSensor.read();
+    motor.setSpeeds(40,40);
+    proxSensor.read();
   }
   // put your main code here, to run repeatedly:
 
