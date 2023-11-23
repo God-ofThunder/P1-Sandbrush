d01fd4558a8c08a28c8ad02215f9a7996673305a
God-ofThunder <thor.borknielsen@gmail.com>
2023-11-23 15:00:42 +0100

    more readeble

--- P1.ino
+++ P1.ino
@@ -9,9 +9,6 @@ Zumo32U4OLED display; //der findes OLED og LCD
 #define NUM_SENSORS 3
 int proxSensorValues[NUM_SENSORS];
 
-int min; // disse værdier kan ændres
-int max = 80;
-
 void printReadinToSerial(){
   char buffer[80];
   sprintf(buffer,"%4d %4d %4d\n",
@@ -25,84 +22,79 @@ void printReadinToSerial(){
  // proxSensor.read(proxSensorValues, QTR_EMITTERS_ON);
 //}
 
-void setup() {
-  Serial.begin(9600);
-  proxSensor.initThreeSensors();
-  
-  proxSensor.setBrightnessLevels(50,500);//første er level, næste er count brightness
-  printReadinToSerial();
-  
+void waitForStartSignal(int *proxSensorReadings){ 
   //buttonA.waitForButton(); //den er god at have med, fordi loopet starter først når der er trykket 
   buttonA.getSingleDebouncedRelease();
   while(buttonA.getSingleDebouncedRelease() == false){
-    proxSensor.read();
-    
-    int proxLeft = proxSensor.countsLeftWithLeftLeds();
-    int proxRight = proxSensor.countsRightWithRightLeds();
-    display.clear();
-    display.gotoXY(0,1);
-    display.print(proxLeft);
-    display.print(" . ");
-    display.print(proxRight);
-    //int proxRight = proxSensor.countsRightWithRightLeds ();
-    //display.clear();
-    //display.gotoXY(0,0); //levels aflæses før press A, når det er sat i whileloop i setup
-    //display.print(proxRight); 
+	  readProxSensors(proxSensorReadings);
+	  printProxSensorReadings(proxSensorReadings);
   } 
-  proxSensor.read();
-  min = proxSensor.countsRightWithRightLeds();
-  
-  //motor.setSpeeds(50,50);
-  // put your setup code here, to run once:
-
 }
 
+void readProxSensors(int *readings){
+	proxSensor.read();
+	readings[0] = proxSensor.countsLeftWithLeftLeds();
+	readings[1] = proxSensor.countsFrontWithRightLeds();
+}
 
+void printProxSensorReadings(int *readings){
+	display.clear();
+	display.gotoXY(0,1);
+	display.print(readings[0]);
+	display.print("--");
+	display.print(readings[1]);
+}
 
-void loop() {
-  proxSensor.read();
-  int proxLeft = proxSensor.countsLeftWithLeftLeds();
-  int proxRight = proxSensor.countsRightWithRightLeds();
-  int proxFrontL = proxSensor.countsFrontWithLeftLeds();
-  int proxFrontR = proxSensor.countsFrontWithRightLeds();
-  display.clear();
-  display.gotoXY(0,1);
-  display.print(proxLeft);
-  display.print(" . ");
-  display.print(proxRight);
-  display.gotoXY(0,0);
-  display.print(proxFrontL);
-  display.print(" . ");
-  display.print(proxFrontR);
-
-  if (proxFrontR >= 182 && proxRight >= 182){
+void shapTurn(){
     motor.setSpeeds(0,0);
     motor.setSpeeds(-60,-60);
     delay(200);
     motor.setSpeeds(-100,100);
     delay(900);
     motor.setSpeeds(60,60);
-    
-  }
+}
 
+void followWall(int *readings, int minDistanse){	
+	printProxSensorReadings(readings);
+	if (readings[1] >= minDistanse){ 
+		motor.setSpeeds(70,60);// lav hastighed giver mindre overslag i ønsket distance (proportionel regulering)
+	}
+	else if (readings[1] < minDistanse){ 
+		motor.setSpeeds(60,70);
+	}
+}
 
-  else if (proxRight >= min){
-    
-    motor.setSpeeds(70,60);// lav hastighed giver mindre overslag i ønsket distance (proportionel regulering)
-   
-  }
+void followEdge(int *readings, int minDistanse){
+	if (readings[1] >= 50 && readings[0] >= 50){
+		shapTurn();
+	}
+	else{
+		followWall(readings, minDistanse);
+	}
+}
 
-  else if (proxRight < min){
-    
-    motor.setSpeeds(60,70);
-    
-  }
-  //else if (proxRight == proxFrontR){
-    //motor.setSpeeds(70,70);
-  //}
-
+void setup() {
+	Serial.begin(9600);
+	proxSensor.initThreeSensors();
   
-  
-  
+	proxSensor.setBrightnessLevels(50,500);//første er level, næste er count brightness
+	printReadinToSerial();
+
+	int proxSensorReadings[2];
+	waitForStartSignal(proxSensorReadings);
+
+	readProxSensors(proxSensorReadings);
+	int minDistanse = proxSensorReadings[1];
+	
+	while(true){
+		readProxSensors(proxSensorReadings);
+		followEdge(proxSensorReadings, minDistanse);
+	}
+	motor.setSpeeds(0,0);
+}
+
+
+
+void loop() {
 
 }
